bb716a253762d2ef449b8266a3acc2b52b18b2ba
Bagas <bagassambega@gmail.com>
2024-03-20 21:04:25 +0700

    feat: implement IDT

--- src/interrupt/idt.h
+++ src/interrupt/idt.h
@@ -33,8 +33,17 @@ extern struct IDTR _idt_idtr;
 struct IDTGate {
     // First 32-bit (Bit 0 to 31)
     uint16_t offset_low;
+    uint16_t segment;
+    uint8_t _reserved : 5;
+    uint8_t _r_bit_1 : 3;
+    uint8_t _r_bit_2 : 3;
+    uint8_t gate_32 : 1;
+    uint8_t _r_bit_3 : 1;
+    uint8_t privilege : 2;
+    uint8_t valid_bit : 1;
+    uint16_t offset_high; 
 
-    // TODO : Implement
+    
 } __attribute__((packed));
 
 /**
@@ -43,7 +52,10 @@ struct IDTGate {
  *
  * ...
  */
-// TODO : Implement
+struct InterruptDescriptorTable {
+    struct IDTGate table[IDT_MAX_ENTRY_COUNT];
+
+} __attribute((packed));
 // ...
 
 /**
@@ -52,7 +64,10 @@ struct IDTGate {
  *
  * ...
  */
-// TODO : Implement
+struct IDTR {
+    uint16_t size;
+    struct InterruptDescriptorTable *address;
+} __attribute__((packed));
 // ...
 
 
