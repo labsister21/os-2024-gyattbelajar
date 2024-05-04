b030e2f008ad47c6a293443b62652d7a9c6ba750
Bagas <bagassambega@gmail.com>
2024-05-04 09:07:32 +0700

    fix: keyboard user mode

--- src/kernel.c
+++ src/kernel.c
@@ -78,13 +78,25 @@ void kernel_setup(void) {
     activate_keyboard_interrupt();
     framebuffer_clear();
     framebuffer_set_cursor(0, 0);
-        
-    // int col = 0;
     keyboard_state_activate();
+    initialize_filesystem_fat32();
+    gdt_install_tss();
+    set_tss_register();
+
+    paging_allocate_user_page_frame(&_paging_kernel_page_directory, (uint8_t*) 0);
     
-    struct BlockBuffer b;
-    for (int i = 0; i < 512; i++) b.buf[i] = i % 16;
-    write_blocks(&b, 17, 1);
+// Write shell into memory
+    struct FAT32DriverRequest request = {
+        .buf                   = (uint8_t*) 0,
+        .name                  = "shell",
+        .ext                   = "\0\0\0",
+        .parent_cluster_number = ROOT_CLUSTER_NUMBER,
+        .buffer_size           = 0x100000,
+    };
+    read(request);
+
+    set_tss_kernel_current_stack();
+    kernel_execute_user_program((uint8_t*) 0);
     
     while (true) {
         keyboard_state_activate();
