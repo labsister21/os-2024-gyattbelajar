62a4f4b9623fe67fff6932e1bd56432e69bb8253
NoHaitch <franciscotrianto@gmail.com>
2024-05-15 18:44:53 +0700

    fix: add comment

--- src/user/syscall.c
+++ src/user/syscall.c
@@ -1,5 +1,18 @@
 #include "syscall.h"
 
+/**
+ * System call 
+ * 0 : read file
+ * 1 : read_directory
+ * 2 : write file / folder
+ * 3 : delete file / empty folder
+ * 4 : get keyboard input
+ * 5 : putchar
+ * 6 : putstring
+ * 7 : active keyboard state
+ * 8 : read cluster
+ * 9 : clear screen
+ */
 void syscall(uint32_t eax, uint32_t ebx, uint32_t ecx, uint32_t edx) {
     __asm__ volatile("mov %0, %%ebx" : /* <Empty> */ : "r"(ebx));
     __asm__ volatile("mov %0, %%ecx" : /* <Empty> */ : "r"(ecx));
--- src/user/syscall.h
+++ src/user/syscall.h
@@ -1,3 +1,16 @@
 #include <stdint.h>
 
+/**
+ * System call 
+ * 0 : read file
+ * 1 : read_directory
+ * 2 : write file / folder
+ * 3 : delete file / empty folder
+ * 4 : get keyboard input
+ * 5 : putchar
+ * 6 : putstring
+ * 7 : active keyboard state
+ * 8 : read cluster
+ * 9 : clear screen
+ */
 void syscall(uint32_t eax, uint32_t ebx, uint32_t ecx, uint32_t edx) ;
