206fc6f0e33a0eaf7833b240789b6b7d959a4898
Yusufarsan <sandiyusufardian@gmail.com>
2024-05-18 17:32:10 +0700

    Fix mkdir so the current_path variable still exist

--- src/user/mkdir.c
+++ src/user/mkdir.c
@@ -4,7 +4,10 @@
 
 // argument is folder name
 void mkdir (char argument[]) {       // Asumsi panjang len harus <= 8
-    int32_t retcode;
+    char curr_path[256];
+    memcpy(curr_path, current_path, 256);
+    strlen(curr_path);
+    int32_t errorCode = 2;
     uint8_t name_len = strlen(argument);
     global_request.buffer_size = 0;
     global_request.buf = buf;
@@ -15,17 +18,22 @@ void mkdir (char argument[]) {       // Asumsi panjang len harus <= 8
     memcpy(global_request.ext, "dir", 3); // Folder extension (dir
     global_request.parent_cluster_number = current_directory;  // Parent cluster number
     memcpy(global_request.name, argument, name_len);   // Folder name
-    syscall(1, (uint32_t) &global_request, (uint32_t) &retcode, 0); // syscall to create folder
-    if (retcode == 0) {     // Folder already exists
+    syscall(1, (uint32_t) &global_request, (uint32_t) &errorCode, 0); // syscall to create folder
+
+    // Buat testing debug
+    // struct FAT32DirectoryTable temp_dir_tab = dir_table;
+
+    if (errorCode == 0) {     // Folder already exists
         put("Folder already exists!\n", BIOS_RED);
-    } else if (retcode == 2) {  // Folder not found
+    } else if (errorCode == 2) {  // Folder not found
         memset(global_request.buf, 0, CLUSTER_SIZE);
-        syscall(2, (uint32_t) &global_request, (uint32_t) &retcode, 0); // syscall to write folder
-        if (retcode == 0) {
+        syscall(2, (uint32_t) &global_request, (uint32_t) &errorCode, 0); // syscall to write folder
+        memcpy(current_path, curr_path, 256);
+        if (errorCode == 0) {
             put("Folder is successfully created!\n", BIOS_LIGHT_GREEN);
         }
         else {
-            put("Failed to create folder! Unknown error\n", BIOS_RED);
+            put("Failed to create folder after trying to write folder! Unknown error\n", BIOS_RED);
         }
     }
     else {
